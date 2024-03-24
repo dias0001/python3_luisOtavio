7e988e2583ff9431f6b01ad597ff329ca1cd51a1
Luiz Gustavo Chaves dias <l.g.chaves.d@gmail.com>
2024-03-24 01:44:48 -0300

    Add files via upload

--- curso_python_1/aula21.py
+++ curso_python_1/aula21.py
@@ -4,17 +4,22 @@
 # and - Todas as condições precisam ser verdadeiras
 # verdadeiras
 # Se qualquer valor for considerado falso, 
-# a expressão inteira será avaliada naquele valr
+# a expressão inteira será avaliada naquele valor
 # São  considerados falsy (que vc já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # USADO PARA REPRESENTAR UM NÃO VALOR
-entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Senha')
 
-senha_permitida = '123456'
+#entrada = input('[E]ntrar [S]air: ')
+#senha_digitada = input('Senha')
 
-if entrada == 'E' and senha_digitada == senha_permitida:
-    print('Entrar')
-else:
-    print('Sair')
+#senha_permitida = '123456'
+
+#if entrada == 'E' and senha_digitada == senha_permitida:
+#    print('Entrar')
+#else:
+#    print('Sair')
+
+# avaliação de curto circuito
+print(True and True and True)
+print(True and 0 and True)
